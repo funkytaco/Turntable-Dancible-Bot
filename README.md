32e2885eb0a42ad20649c82ffdf0a97f98083807
Luis Gonzalez <lgonzal@redhat.com>
2021-03-06 14:11:01 -0500

    Turntable updates for 2021 of README and bot settings.

--- README.md
+++ README.md
@@ -4,10 +4,41 @@ A very basic, customizable Turntable.fm bot written in javascript for Node.js ut
 
 ## Installation
 
-Just run the following:
-node /path/to/bot.js
+### Git Clone This Repo
+`git clone https://github.com/funkytaco/Turntable-API-Taco-Bot`
+`cd Turntable-API-Taco-Bot`
+### Install NodeJS > 12.x and Turntable API
+`npm install nodejs`
+`npm install ttapi`
 
-make sure bot_settings.js has your authentication settings for turntable.fm
+### Join The Chat Room
+Every bot needs it's own account.
+
+Once you've logged into Turntable.FM with the account you will use for the bot, click this bookmarklet:
+
+http://alaingilbert.github.io/Turntable-API/bookmarklet.html
+
+*    Drag the button in your bookmarks bar.
+*   Browse to turntable.fm and click it.
+* Use the three variables in the next step below.
+
+#Copy the bookmarklet output into bot_settings.js
+
+Paste the three variables obtained from the bookmarklet (USERID, ROOMID, AUTH) into botsettings.js *at the top of the bot_settings.js file*.
+
+### Launch NodeJS
+
+Just run the following to start the bot:
+node ./bot.js
+
+* make sure bot_settings.js has your authentication settings for turntable.fm
+
+Your bot should join the room and say hello. That's it!
+
+# Additional Info
+You should browse through bot_settings.js to toggle settings. 
+
+NOTE: **1 = YES, 0 = NO.**
 
 ## Features
 
@@ -22,8 +53,10 @@ The bot can:
 
 
 
-## Help
+## Resources
+* Getting the ID's without the bookmarklet https://github.com/alaingilbert/Turntable-API/wiki/How-to-find-the:-auth,-userid-and-roomid
 
-I'm usually hanging out in the [I ♥ The 80's](http://turntable.fm/i_the_80s) room on Turntable.
+
+I'm usually hanging out in the [Dancible](http://turntable.fm/dancible) room on Turntable.
 If I'm on Turntable, I'm usually working, so please try getting it to work before contacting me.
 
--- bot_settings.js
+++ bot_settings.js
@@ -1,22 +1,27 @@
 var settings = exports;
-settings.AUTH = '_AUTH_USER_HERE_READ_URL_TO_THE_RIGHT_'; //README: https://github.com/alaingilbert/Turntable-API/wiki/How-to-find-the:-auth,-userid-and-roomid
-settings.USERID = '_REPLACE_WITH_USER_ID_';
-settings.ROOMID = '_REPLACE_WITH_ROOM_ID_';
-settings.BOT_NAME = 'Taco Bot';
-settings.BOT_SHORTNAME = 'taco';
-settings.RULES_URL = 'http://www.example.com/rules_page_here.html'
-settings.BOT_MODERATORS_ARRAY = ['4f671b49590ca246ed01db9b','4f9b545caaa5cd2af400022f']; //Bot Moderators - Put your user id's here. Room mods will be able to mod your bot.
-settings.ROOM_MODS_ARE_BOT_ADMINS = 0; //Allow room mods to automatically be bot admins? todo
+/** BEGIN PASTE OF BOOKMARKLET VARIABLES **/
+
+/** END PASTE OF BOOKMARKLET VARIABLES **/
+
+//DO NOT EDIT THIS SECTION - SEE "ADDITIONAL INFO" IN README
+settings.AUTH = AUTH; 
+settings.USERID = USERID; 
+settings.ROOMID = ROOMID; 
+
+//OPTIONAL SETTINGS & CUSTOMIZATIONS
+settings.BOT_NAME = 'DJ Funky Taco Bot';
+settings.BOT_SHORTNAME = 'tacobot';
+settings.RULES_URL = 'http://www.dancible.com/events/rules.html'
+settings.BOT_MODERATORS_ARRAY = ['4f671b49590ca246ed01db9b', USERID ]; //NOTE: Every bot needs a user account.
+//settings.ROOM_MODS_ARE_BOT_ADMINS = 0; //Allow room mods to automatically be bot admins? todo
 settings.DEBUG_LOGS = 1; //enable printing to console log.
 settings.VOTE_POLLING = 1;
-settings.AUTO_AWESOME = 0; //not implemented: http://faq.turntable.fm/customer/portal/articles/258935
+settings.AUTO_AWESOME = 0; 
 settings.GREET_ON_ENTER = 1;
 settings.GREET_ON_EXIT = 0;
 settings.TALK_IN_CHAT = 1;
 settings.TALK_IN_PRIVATE = 1;
-//not configured yet
-settings.DJ_ON = '/dj win';
-settings.DJ_OFF = '/dj win off';
+
 //settings.DJ_ANNOUNCEMENTS = TRUE;
 settings.ADDME = '*addme'; //todo
 settings.REMOVEME = '*removeme'; //todo
